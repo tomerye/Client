5219cb64153022315c3550152ae363596accb69f
Tomer Yechiel <tomer.yechiel1@gmail.com>
2013-04-15 00:17:31 +0300

    init commit

---
+++ src/Client.cpp
@@ -0,0 +1,18 @@
+/*
+ * Client.cpp
+ *
+ *  Created on: Apr 7, 2013
+ *      Author: tomer
+ */
+
+#include "Client.h"
+
+Client::Client() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Client::~Client() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Client.h
@@ -0,0 +1,20 @@
+/*
+ * Client.h
+ *
+ *  Created on: Apr 7, 2013
+ *      Author: tomer
+ */
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+class Client {
+public:
+	Client();
+	virtual ~Client();
+
+private:
+
+};
+
+#endif /* CLIENT_H_ */
