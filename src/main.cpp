ae6c040004ff6928462f510f15392feabef54353
Tomer Yechiel <tomer.yechiel1@gmail.com>
2013-05-09 20:29:02 +0300

    add loop for many sends

--- src/main.cpp
+++ src/main.cpp
@@ -16,24 +16,23 @@
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
 
-
-
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		return 1;
 	}
 
 	boost::asio::io_service io_service;
-	boost::thread t(boost::bind(&boost::asio::io_service::run, &io_service));
-	Client client(io_service, "127.0.0.1", "55555", std::atoi(argv[1]));
 //	boost::thread t(boost::bind(&boost::asio::io_service::run, &io_service));
+	Client client(io_service, "127.0.0.1", "55555", std::atoi(argv[1]));
+	boost::thread t(boost::bind(&boost::asio::io_service::run, &io_service));
 //	io_service.run();
 	Packet p;
 	p.id_ = 333;
 	p.file_path_ = "sfdsf";
 	p.opcode_ = "ddd";
-	sleep(10);
-	client.sendPacket(p);
+	char line[10];
+	while (std::cin.getline(line, 10))
+		client.sendPacket(p);
 	sleep(10);
 //	t.join();
 //	io_service.run();
