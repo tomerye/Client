4677f06ea1e8447306ee6bd648c13a62c4d74c6c
Tomer Yechiel <tomer.yechiel1@gmail.com>
2013-05-20 13:49:25 +0300

    add gpio class

---
+++ src/Gpio.h
@@ -0,0 +1,21 @@
+///*
+// * Gpio.h
+// *
+// *  Created on: May 16, 2013
+// *      Author: tomer
+// */
+//
+//#ifndef GPIO_H_
+//#define GPIO_H_
+//
+//class Gpio {
+//public:
+//	Gpio();
+//	virtual ~Gpio();
+//	int init(unsigned int gpioNum);
+//
+//
+//
+//};
+//
+//#endif /* GPIO_H_ */
